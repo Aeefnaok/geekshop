e893fa08812eb885f299cbf45c3c4b6768d77104
Ivan Chupriyanov <Aerocoolbye@yandex.ru>
2021-11-25 20:28:44 +0700

    not complete

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {'title': 'Магазин'}
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
@@ -11,5 +12,29 @@ def products(request):
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
+
+
+def context(request):
+    context = {
+        'title': 'test context',
+        'header': 'Добро пожаловать на сайт',
+        'username': 'Ivan',
+        'products': [
+            {'name': 'Стулья', 'price': 6789},
+            {'name': 'Диваны', 'price': 12389},
+            {'name': 'Столы', 'price': 16789},
+        ],
+    }
+    return render(request, "mainapp/test_context.html", context)
+
+
+def menu(request):
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+    return render(request, 'inc_categories_menu.html', links_menu)
 
 # Create your views here.
